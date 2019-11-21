c3a3d4e7d4eadac19c776e6d64c41e5829111b25
DindaMajesty <dindamesti@gmail.com>
2019-11-21 15:51:26 +0700

    add class MataUang

--- kelas.py
+++ kelas.py
@@ -1,4 +1,6 @@
-def Anguilla():
-    idr = 10000000
-    hasil = float(idr) * 0.00019
-    return hasil
+class MataUang:
+
+    def Anguilla(self):
+        idr = 10000000
+        hasil = float(idr) * 0.00019
+        return hasil
