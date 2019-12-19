63e5cdb26ff38c9ef31edfbabe7f8b366cdf8aa9
trianggadios <trianggadio@gmail.com>
2019-12-19 16:07:40 +0700

    selenium

--- kelas_2b/trianggadios.py
+++ kelas_2b/trianggadios.py
@@ -16,13 +16,7 @@ class TriAngga(object):
             read = csv.reader(toto_wekwek, delimiter=',')
             for row in read:
                 if row[0] == self.keyword:
-                    options = webdriver.ChromeOptions()
-                    options.binary_location = '/usr/bin/chromium-browser'
-                    options.add_argument("--no-sandbox")
-                    options.add_argument("--no-default-browser-check")
-                    options.add_argument("--no-first-run")
-                    options.add_argument("--disable-default-apps")
-                    driver = webdriver.Chrome('/home/travis/virtualenv/python3.6/chromedriver',chrome_options=options)
+                    driver = webdriver.Chrome()
                     driver.get(row[1])
                     sleep(1)
                     driver.close()
