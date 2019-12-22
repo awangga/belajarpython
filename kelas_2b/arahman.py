db85f57b253b718632564119130c99d823d54791
aditya <mas.adityarahman@gmail.com>
2019-12-22 14:41:07 +0700

    1184021

--- kelas_2b/arahman.py
+++ kelas_2b/arahman.py
@@ -2,7 +2,7 @@ import csv
 
 
 class Jadwal(object):
-    with open('bahannya/adit.csv', 'r') as csvfile:
+    with open('kelas_2b/adit.csv', 'r') as csvfile:
         tumang = csv.reader(csvfile, delimiter=',')
         for row in tumang:
             print("Informasi jadwal Kereta :", row[0], row[1], row[2])
