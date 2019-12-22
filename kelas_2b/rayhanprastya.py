994957e2e41bfe3d0b7848696c1ec45a5de8a501
rayhan <rayhan.prastya@gmail.com>
2019-12-22 23:10:52 +0700

    rayprastya 1184069

--- kelas_2b/rayhanprastya.py
+++ kelas_2b/rayhanprastya.py
@@ -3,6 +3,6 @@ import csv
 
 class rayprastya(object):
     def convert(self):
-        read_file = pd.read_csv (r'belajarpython\kelas_2b\raypras.csv')
-        read_file.to_excel (r'belajarpython\kelas_2b\raypras.xlsx', index = None, header=True)
+        read_file = pd.read_csv (r'..\kelas_2b\raypras.csv')
+        read_file.to_excel (r'..\kelas_2b\raypras.xlsx', index = None, header=True)
         print("file berhasil dirubah")
