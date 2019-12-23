226562681116af6086e35cfc23405d8a8ca87a8c
Tri Angga Dio Simamora <trianggadio@gmail.com>
2019-12-23 22:06:09 +0700

    Revert "1184109"

--- kelas_2b/jenly.py
+++
@@ -1,8 +0,0 @@
-import csv
-class Tadi_salah_pak_saya_kira_boleh_pake_inputan_user(object):
-
-    def Penjumlahan_yang_terencana(self):
-        with open('kelas_2b/penjumlahan.csv') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            for row in csv_reader:
-                print(f'\t{row[0]} + {row[1]} = {row[2]}.')
