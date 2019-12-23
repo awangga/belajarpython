72ff0e04bef0ae6ceacb02d499f3b151d3f3f62d
Tari <tarippu@gmail.com>
2019-12-23 23:59:00 +0700

    1184039

--- kelas_2b/tarippu.py
+++ kelas_2b/tarippu.py
@@ -1,12 +1,8 @@
 import csv
 
-class Tari:
+class Tari():
     def main(self):
         with open('./kelas_2b/tarippu.csv') as csv_files:
             reader=csv.reader(csv_files)
             for kartu in reader:
-                print(kartu)
-                if kartu == 1:
-                    print('tidak ada apa-apa')
-                else:
-                    print('ini',kartu[0], 'dengan tipe',kartu[1])
+                print (str(kartu[0][-3:]))
